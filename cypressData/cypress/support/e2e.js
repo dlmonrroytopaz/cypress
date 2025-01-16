450b5f0af333fb6c022cee4f8cbd4ccefd78060a
Diego Gagliano <diegog@topsystemscorp.com>
2025-01-16 12:32:35 -0300

    hola

--- cypressData/cypress/support/e2e.js
+++
@@ -1,9 +0,0 @@
-// ***********************************************************
-// Aquí puedes agregar código para configuración y comandos personalizados si lo necesitas
-//
-// ***********************************************************
-// Import commands.js using ES2015 syntax:
-import './commands';  // Esto asegura que Cypress cargue el archivo de comandos
-import 'cypress-mochawesome-reporter/register';
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
--- cypressData/cypress/support/index.js
+++
@@ -1,3 +0,0 @@
-// Test/cypress/support/index.js
-import './commands';  // Esto asegura que Cypress cargue el archivo de comandos
-//import '@shelex/cypress-allure-plugin';
