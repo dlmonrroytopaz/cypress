56792cd1fa584b7f58dd9f708077c23ffdbc6613
Diego Gagliano <diegog@topsystemscorp.com>
2025-01-16 16:09:54 -0300

    test 16

--- cypress/e2e/specs/test_report_mocha2.cy.js
+++ cypress/e2e/specs/test_report_mocha2.cy.js
@@ -4,8 +4,5 @@ describe('Mi prueba con Mochawesome', () => {
     cy.get('h1').should('contain', 'Example Domain');
   });
 
-  it('deberiafallarestetest', () => {
-  cy.visit('https://www.example.com');
-  cy.get('h1').should('contain', 'Texto Inexistente');  // Esto fallará
-  });
+
 });
