4a75b67d1e1ad565e125bc08fed576206611502c
Diego Gagliano <diegog@topsystemscorp.com>
2025-01-16 14:36:17 -0300

    test 12

--- cypress.config.js
+++ cypress.config.js
@@ -11,10 +11,12 @@ module.exports = defineConfig({
   },
   e2e: {
     
-    projectId: 'wt9vj9',
+    
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
+      config.projectId = 'wt9vj9',
       require('cypress-mochawesome-reporter/plugin')(on);
+      
     },
     env: {
       CYPRESS_RECORD_KEY: '8f461d36-95f5-477b-bc24-255909b2998b',
