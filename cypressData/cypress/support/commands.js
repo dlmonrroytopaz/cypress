a1c26f6c22e4ae8085905c049c4b1f3ac387dc81
Diego Gagliano <diegog@topsystemscorp.com>
2025-01-16 12:04:26 -0300

    hola

--- cypressData/cypress/support/commands.js
+++ cypressData/cypress/support/commands.js
@@ -1,54 +1,8 @@
-// En support/commands.js
-//Comando para pruebas
-Cypress.Commands.add('testCommand', () => {
-  cy.log('Test command executed');
-});
-
-// Comando Allure - configuración reporte
-//Cypress.Commands.add('logReportData', (feature, story) => {
- // cy.allure().feature(feature);
- // cy.allure().story(story);
-//});
-
-// Comando Axios - TOKEN
-import axios from 'axios';
-
-// Comando para hacer login y guardar el token en localStorage
-Cypress.Commands.add('login', () => {
-  const credentials = {
-    username: 'demo',  // Usuario ficticio
-    password: 'password',  // Contraseña ficticia
-  };
-
-  return cy.request({
-    method: 'POST',
-    url: 'https://jsonplaceholder.typicode.com/posts',  // URL de ejemplo
-    body: credentials,  // Usamos un cuerpo de ejemplo para simular autenticación
-    failOnStatusCode: false  // No fallar si el código de estado es 401
-  }).then((response) => {
-    const token = 'fake-token-123456789';  // Este token sería real en un entorno real
-    window.localStorage.setItem('auth_token', token);  // Guardamos el token en localStorage
-  });
-});
-
-// Comando para obtener datos protegidos usando el token
-Cypress.Commands.add('getProtectedData', () => {
-  const token = window.localStorage.getItem('auth_token');  // Leemos el token del localStorage
-  return cy.request({
-    method: 'GET',
-    url: 'https://jsonplaceholder.typicode.com/posts/1',  // Ruta protegida
-    headers: {
-      Authorization: `Bearer ${token}`,  // Incluimos el token en los headers
-    },
-    failOnStatusCode: false,  // Permitimos manejar manualmente errores como 401
-  });
-});
-
-// Pruebas TOKEN con GitHub
+// Comando para login con GitHub y guardar el token en localStorage
 Cypress.Commands.add('loginGitHub', () => {
-  const token = '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';  // Sustituye por tu token de GitHub
+  const token = process.env.GITHUB_TOKEN_SECRET;
 
-  // Comprobamos si el token existe antes de guardarlo
+  // Verificamos si el token existe antes de guardarlo
   if (!token) {
     cy.log('¡Error! El token de GitHub no está presente.');
     throw new Error('Token de GitHub no encontrado.');
@@ -59,6 +13,7 @@ Cypress.Commands.add('loginGitHub', () => {
   cy.log('Token guardado en localStorage');
 });
 
+// Comando para obtener el perfil de GitHub usando el token guardado
 Cypress.Commands.add('getGitHubProfile', () => {
   const token = window.localStorage.getItem('auth_token');  // Obtenemos el token desde localStorage
 
