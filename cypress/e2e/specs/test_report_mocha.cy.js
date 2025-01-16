a02b6f0c673338a13694e09693882912bba1e3be
Diego Gagliano <diegog@topsystemscorp.com>
2025-01-16 14:24:18 -0300

    test11

--- cypress/e2e/specs/test_report_mocha.cy.js
+++ cypress/e2e/specs/test_report_mocha.cy.js
@@ -4,10 +4,7 @@ describe('MipruebaconMochawesome', () => {
     cy.get('h1').should('contain', 'Example Domain');
   });
 
-  it('deberiafallarestetest', () => {
-    cy.visit('https://www.example.com');
-    cy.get('h1', { timeout: 8000 }).should('contain', 'Texto Inexistente');  // Esto fallará
-  });
+  
 
   
 
